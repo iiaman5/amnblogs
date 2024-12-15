ca09f18ae07a093cc4f440f02f7e23d908a5c12b
Aman jangra <JANGRAAMAN942@GMAIL.COM>
2024-12-15 13:52:01 +0530

    Update rewrite.js

--- netlify/edge-functions/rewrite.js
+++ netlify/edge-functions/rewrite.js
@@ -1,10 +1,1 @@
-const rewrite = async (request, context) => {
-    const path = context.geo?.country?.code === 'AU' ? '/edge/australia' : '/edge/not-australia';
-    return new URL(path, request.url);
-};
 
-export const config = {
-    path: '/edge'
-};
-
-export default rewrite;
